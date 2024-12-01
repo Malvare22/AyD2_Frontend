bb0bbc6d372fde65f78d5e1bc324340f00597d41
JorgeMarles <jamarlesf@gmail.com>
2024-12-01 18:31:19 -0500

    fix: dynamic profile

--- src/pages/perfil.tsx
+++ src/pages/perfil.tsx
@@ -1,6 +1,8 @@
 import { useNavigate } from "react-router-dom";
 import Button from "../components/button";
 import LayoutLogin from "../layouts/layoutLogin";
+import { useEffect, useState } from "react";
+import { Usuario } from "../services/usuarioService";
 
 function Perfil() {
   const navigate = useNavigate();
@@ -34,35 +36,58 @@ function Perfil() {
 }
 
 const Contain = () => {
+  const [user, setUser] = useState<Usuario>({
+    apellidos: '',
+    codigo: '',
+    correo: '',
+    id: 1,
+    nombres: '',
+    rol: 'estudiante'
+  });
+
+  useEffect(() => {
+    setUser(JSON.parse(localStorage.getItem('USER')!))
+  }, [])
+
   return (
     <div className="md:my-24 mb-0">
       <div className="text-4xl text-center font-bold my-10">Perfil</div>
       <div className="md:w-10/12 mx-auto border border-black rounded-lg shadow-lg p-6 text-xl bg-white mb-24">
         {/** Cabecera estética con fondo limpio */}
-        <div className="grid grid-cols-2 gap-y-4 items-center p-10 space-y-8">
+        <div className="items-center p-10 space-y-8">
           {/** Etiquetas alineadas y columnas con estructura */}
-          <div className="font-semibold text-bolder text-right pr-4 mt-8">
-            Código:
+          <div className="flex flex-row">
+            <div className="font-semibold text-bolder pr-4 mt-8">
+              Código:
+            </div>
+            <div className="text-gray-800">{user.codigo}</div>
           </div>
-          <div className="text-gray-800">12345</div>
 
-          <div className="font-semibold text-bolder text-right pr-4">
-            Nombre:
+          <div className="flex flex-row">
+            <div className="font-semibold text-bolder pr-4">
+              Nombre:
+            </div>
+            <div className="text-gray-800">{user.nombres}</div>
           </div>
-          <div className="text-gray-800">Juan</div>
 
-          <div className="font-semibold text-bolder text-right pr-4">
-            Apellido:
+          <div className="flex flex-row">
+            <div className="font-semibold text-bolder pr-4">
+              Apellido:
+            </div>
+            <div className="text-gray-800">{user.apellidos}</div>
           </div>
-          <div className="text-gray-800">Pérez</div>
 
-          <div className="font-semibold text-bolder text-right pr-4">
-            Correo:
+          <div className="flex flex-row">
+            <div className="font-semibold text-bolder pr-4">
+              Correo:
+            </div>
+            <div className="text-gray-800">{user.correo}</div>
           </div>
-          <div className="text-gray-800">juan.perez@example.com</div>
 
-          <div className="font-semibold text-bolder text-right pr-4">Rol:</div>
-          <div className="text-gray-800">Administrador</div>
+          <div className="flex flex-row">
+            <div className="font-semibold text-bolder pr-4">Rol:</div>
+            <div className="text-gray-800">{user.rol}</div>
+          </div>
         </div>
       </div>
     </div>
